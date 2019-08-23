b2769f82c30f27c46837db4bd6a5ff57b17ea80c
taitai9847 <taitai9847@gmail.com>
2019-08-23 14:22:16 +0900

    Initial Commit

---
+++ Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by 石川　太洋 on 2019/08/23.
+//  Copyright © 2019 石川　太洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MapTests/MapTests.swift
@@ -0,0 +1,34 @@
+//
+//  MapTests.swift
+//  MapTests
+//
+//  Created by 石川　太洋 on 2019/08/23.
+//  Copyright © 2019 石川　太洋. All rights reserved.
+//
+
+import XCTest
+@testable import Map
+
+class MapTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
